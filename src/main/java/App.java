7a3c5e3850d0b7721af14b1b3a0664a923105b76
Steve Zaske <github@zaske.com>
2017-07-06 00:12:55 -0700

    add adv velocity templates and layout String

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,21 +1,32 @@
 import java.util.HashMap;
-
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
-
 import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
     // This sets the default location for files 
     staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
 
     get("/", (request, response) -> {
-          return new ModelAndView(new HashMap(), "templates/hello.vtl");
-        }, new VelocityTemplateEngine());
+      // spark create a hashmap named model
+      Map<String, Object> model = new HashMap<String, Object>();
+      //then puts the hello.vtl object into the hashmap, with the key of 'template'
+      model.put("template", "templates/hello.vtl" );
+      //this tells spark to return a new HTML file or ModelAndView, using the model hashmap(MODEL) and layout view
+      //this prompts Spark to load our layout template
+      // which triggers the #parse( $template )
+      // The template word refers to the key in the hashmap, which returns the hello.vtl object in the hashmap
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
     get("/favorite_photos", (request, response) -> {
-      return new ModelAndView(new HashMap(), "templates/favorite_photos.vtl");
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/favorite_photos.vtl" );
+      return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
   }
